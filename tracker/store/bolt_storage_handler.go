da6b10beafdd4ebb9263a80877404eb48eac055d
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2026-03-09 12:41:57 +0100

    db fix

--- tracker/store/bolt_storage_handler.go
+++ tracker/store/bolt_storage_handler.go
@@ -104,7 +104,7 @@ type BoltStorageHandler struct {
 	db     *bolt.DB
 }
 
-var _ StorageHandler = &BoltStorageHandler{}
+var _ StorageHandler = (*BoltStorageHandler)(nil)
 
 // EventRecord represents a stored event with metadata
 type EventRecord struct {
